f5b705784d3c73b87e8c15b04bd017bd7fab392b
SerjiVutinss <SerjiVutinss@gmail.com>
2018-04-09 15:34:34 +0100

    Added firebase-config-sample.ts

---
+++ src/firebase-config-sample.ts
@@ -0,0 +1,14 @@
+// Make a copy of this file and rename it to 'firebase-config.ts'.
+//   *Ensure that firebase-config.ts is placed in the src directory
+// Paste your config from your Firebase console below:
+
+export const firebaseConfig = {
+    fire: {
+        apiKey: "your-api-key",
+        authDomain: "your-app.firebaseapp.com",
+        databaseURL: "https://your-database.firebaseio.com",
+        projectId: "your-project-id",
+        storageBucket: "",
+        messagingSenderId: "your-message-sender-id"
+    }
+};
