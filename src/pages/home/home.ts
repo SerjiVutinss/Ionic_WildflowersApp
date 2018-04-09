aed948e59d6e501d3ffd621ea573225dcb576f01
SerjiVutinss <SerjiVutinss@gmail.com>
2018-04-09 16:04:59 +0100

    Created auth methods in home.ts, added empty LoginPage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { AfAuthProvider } from '../../providers/af-auth/af-auth';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  // injecting AlertController and AngularFireAuth provider
+  constructor(
+    private navCtrl: NavController,
+    private alertCtrl: AlertController,
+    private afAuthService: AfAuthProvider
+  ) { }
 
+  // sign out the current user and navigate to the login page
+  login() {
+    this.afAuthService.signOut();
+    this.navCtrl.setRoot(LoginPage);
+  }
+
+  // sign out the current user and reload the home page
+  logout() {
+    this.afAuthService.signOut();
+    this.navCtrl.setRoot(HomePage);
+  }
+
+  // used for testing if a user is authenticated
+  // TODO: wil be expanded upon and reused later
+  openUserPage() {
+    if (this.isAuthenticated()) {
+      // only navigate here if a user is logged in
+      this.navCtrl.push("UserPage");
+    } else {
+      // otherwise, display an alert
+      let alert = this.alertCtrl.create({
+        title: 'Access denied!',
+        subTitle: 'You must be logged in!',
+        buttons: ['OK']
+      });
+      alert.present();
+    }
+
+  }
+
+  // return true if a user is logged in
+  isAuthenticated(): boolean {
+    return this.afAuthService.authenticated;
   }
 
 }
